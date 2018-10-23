4118fed775706b4972b821401814f44b087e9adc
gkalra <gkalra@pdxinc.com>
2018-10-23 20:09:58 +0530

    maven jenkin integration code commited to git

---
+++ MavenProject/src/MavenTestNGPackage/NewTest.java
@@ -0,0 +1,10 @@
+package MavenTestNGPackage;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void TestNGMethod() {
+	  System.out.println("message from Maven testNG class1");
+  }
+}
